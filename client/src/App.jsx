e15aaa6b501f74961c7384b6a7725bebd1149f99
Dagnu <dagnud10@gmail.com>
2026-07-10 01:15:51 -0700

    update in landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-// client/src/App.jsx
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider, useAuth } from './context/AuthContext';
